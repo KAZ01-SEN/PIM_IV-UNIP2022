a15c8a0da72a33bd353266058eb9f61ed74a0604
Luis Eduardo <88053056+LuisEdurdoMDSilva@users.noreply.github.com>
2022-11-10 08:38:10 -0300

    update_save_file

--- main.c
+++ main.c
@@ -1,7 +1,9 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
+#include<stdbool.h>
 #include<locale.h> // biblioteca para uso de acentuação - idioma (Pt-Br)
+#include <time.h> // Biblioteca para cálculos de tempo.
 #include <conio.h>
 
 #define TAM 600
@@ -9,9 +11,61 @@
 struct dados_pessoas
 {
     char inf0[100], inf1[100], inf2[100], inf3[100], inf4[100], inf5[100], inf6[100], inf7[100];
+    int dataD,dataM,dataA,diagD,diagM,diagA;
 };
 
 typedef struct dados_pessoas dados;
+
+bool checkBissexto(int ano) //Função para checar anos bissextos. Não precisa ser chamada fora da função checkDataErr.
+{
+    bool flag = false;
+    if (ano % 100 == 0)
+    {
+        if (ano % 400 == 0)
+        {
+            flag = true;
+        }
+    }
+    else if (ano % 4 == 0)
+    {
+        flag = true;
+    }
+    return (flag);
+}
+
+bool checkDataErr (int dia, int mes, int ano, int anoatual) //Função para checar datas inválidas. Chama a função checkBissexto recursivamente.
+{
+    bool flag = false;
+    bool bs = checkBissexto(anoatual);
+    if ( !(dia>=1&&dia<=31) || !(mes>=1&&mes<=12) || ano>anoatual || (dia>28 && mes==2 && bs==false) || (dia>29 && mes==2) ||
+        (dia>30 &&(mes!=4||mes!=6||mes!=9||mes!=11)) )
+    {
+        flag = true;
+    }
+    return (flag);
+}
+
+int checkIdade (int dia, int mes, int ano, int diaatual, int mesatual, int anoatual) //Função para calcular idade.
+{
+    int idade;
+    idade = anoatual - ano;
+    if (mesatual > mes)
+    {
+
+    }
+    else if (mesatual == mes)
+    {
+        if (diaatual < dia)
+        {
+            idade--;
+        }
+    }
+    else
+    {
+        idade--;
+    }
+    return (idade);
+}
 
 int main()
 {
@@ -20,6 +74,9 @@ int main()
     dados lista[TAM];
 
     int menu=1, close=0, endprog=0, senha=0, i=0, quant=0, comorb=0;
+    bool err=false;
+    time_t ts;
+    struct tm *ct;
 
     char usuario[100];
 
@@ -59,11 +116,16 @@ int main()
                 {
                 case 1:
                     system("cls");
+
+                    FILE *pont_arq;
+                    FILE *com_arq;
+
                     printf("\n\n QUANTOS CADASTROS DESEJA REALIZAR? \n");
                     scanf("%d", &quant);
 
                     for(i=1; i<=quant; i++)
                     {
+                        err=false;
                         system("cls");
                         printf("\n\n\t\t\t      INICIANDO CADASTRO %d\n", i);
                         printf("\t\t\t       ----------------------------\n\n");
@@ -83,17 +145,79 @@ int main()
                         fflush(stdin);
                         scanf("%[^\n]s", lista[i].inf3);
 
-                        printf("\n DATA/NASCIMENTO:");
+                        printf("\n CEP:");
                         fflush(stdin);
                         scanf("%[^\n]s", lista[i].inf4);
+
+                        ts = time(NULL);
+                        ct = localtime(&ts);
+
+                        do{
+                            printf("\n DATA DE NASCIMENTO (DD/MM/AAAA): ");
+                            fflush(stdin);
+                            scanf("%d/%d/%d", &lista[i].dataD,&lista[i].dataM,&lista[i].dataA);
+                            err = checkDataErr(lista[i].dataD,lista[i].dataM,lista[i].dataA,ct->tm_year+1900);
+                            if ( err == true )
+                            {
+                                printf("\n DATA INVÁLIDA! INSIRA NOVAMENTE!");
+                            }
+
+                        }while(err==true);
 
                         printf("\n E-MAIL:");
                         fflush(stdin);
                         scanf("%[^\n]s", lista[i].inf5);
 
-                        printf("\n DATA/DIAGNÓSTICO:");
-                        fflush(stdin);
-                        scanf("%[^\n]s", lista[i].inf6);
+                        do{
+                            printf("\n DATA DE DIAGNÓSTICO (DD/MM/AAAA): ");
+                            fflush(stdin);
+                            scanf("%d/%d/%d", &lista[i].diagD,&lista[i].diagM,&lista[i].diagA);
+                            err = checkDataErr(lista[i].diagD,lista[i].diagM,lista[i].diagA,ct->tm_year+1900);
+                            if ( err == true )
+                            {
+                                printf("\n DATA INVÁLIDA! INSIRA NOVAMENTE!");
+                            }
+
+                        }while(err==true);
+
+                        if(pont_arq == NULL)
+                        {
+                            printf("ERRO NA ABERTURA DO ARQUIVO");
+                            return 1;
+                        }
+
+                        pont_arq= fopen("arquivo.txt", "a");
+
+                        fprintf(pont_arq, "\n\n------------------------------------------\n\n");
+                        fprintf(pont_arq, "\n NOME DO PACIENTE: %s\n", lista[i].inf0);
+                        fprintf(pont_arq, "\n CPF: %s\n", lista[i].inf1);
+                        fprintf(pont_arq, "\n TELEFONE: %s\n", lista[i].inf2);
+                        fprintf(pont_arq, "\n ENDERECO: %s\n", lista[i].inf3);
+                        fprintf(pont_arq, "\n CEP: %s\n", lista[i].inf4);
+                        fprintf(pont_arq, "\n DATA DE NASCIMENTO: %d/%d/%d\n", lista[i].dataD,lista[i].dataM,lista[i].dataA);
+                        fprintf(pont_arq, "\n IDADE: %d\n", checkIdade(lista[i].dataD,lista[i].dataM,lista[i].dataA,ct->tm_mday,ct->tm_mon+1,ct->tm_year+1900));
+                        fprintf(pont_arq, "\n EMAIL: %s\n", lista[i].inf5);
+                        fprintf(pont_arq, "\n DATA DO DIAGNÓSTICO: %d/%d/%d\n", lista[i].diagD,lista[i].diagM,lista[i].diagA);
+
+
+                        if (checkIdade >= 65)
+                                {
+                                    strcpy(lista[i].inf7,"N\\A");
+                                    if(com_arq == NULL)
+                                    {
+                                        printf("ERRO NA ABERTURA DO ARQUIVO");
+                                        return 1;
+                                    }
+
+                                    com_arq= fopen("comorbidade.txt", "a");
+
+                                    fprintf(com_arq, "\n NOME DO PACIENTE: %s\n", lista[i].inf0);
+                                    fprintf(com_arq, "\n CEP: %s\n", lista[i].inf4);
+                                    fprintf(com_arq, "\n IDADE: %d\n", checkIdade(lista[i].dataD,lista[i].dataM,lista[i].dataA,ct->tm_mday,ct->tm_mon+1,ct->tm_year+1900));
+                                    fprintf(com_arq, "\n COMORBIDADE: %s\n", lista[i].inf7);
+                                    fprintf(com_arq, "\n\n------------------------------------------\n\n");
+
+                                }
 
                         do
                         {
@@ -108,11 +232,26 @@ int main()
                                 printf("\n COMORBIDADES:");
                                 fflush(stdin);
                                 scanf("%[^\n]s", lista[i].inf7);
+
+                                fprintf(pont_arq, "\n COMORBIDADE: %s\n", lista[i].inf7);
+                                fclose (pont_arq);
+
+                                fprintf(com_arq, "\n NOME DO PACIENTE: %s\n", lista[i].inf0);
+                                fprintf(com_arq, "\n CEP: %s\n", lista[i].inf4);
+                                fprintf(com_arq, "\n IDADE: %d\n", checkIdade(lista[i].dataD,lista[i].dataM,lista[i].dataA,ct->tm_mday,ct->tm_mon+1,ct->tm_year+1900));
+                                fprintf(com_arq, "\n COMORBIDADE: %s\n", lista[i].inf7);
+                                fprintf(com_arq, "\n\n------------------------------------------\n\n");
+                                fclose (com_arq);
                                 break;
 
                             case 2:
+
                                 strcpy(lista[i].inf7,"N\\A");
+                                fprintf(pont_arq, "\n COMORBIDADE: %s\n", lista[i].inf7);
+                                fprintf(pont_arq, "\n\n------------------------------------------\n\n");
+                                fclose (pont_arq);
                                 break;
+
 
                             default:
                                 system("cls");
@@ -134,45 +273,23 @@ int main()
 
                     for(i=1; i<=quant; i++)
                     {
-                        FILE *pont_arq;
 
-                        if(pont_arq == NULL)
-                        {
-                            printf("ERRO NA ABERTURA DO ARQUIVO");
-                            return 1;
-                        }
+                        system("cls");
+                        printf("\n\n\t\t\t\t      EXIBINDO CADASTRO %d\n", i);
+                        printf("\t\t\t       --------------------------------\n\n");
+                        printf(" NOME DO PACIENTE: %s\n", lista[i].inf0);
+                        printf("\n CPF: %s\n", lista[i].inf1);
+                        printf("\n TELEFONE: %s\n", lista[i].inf2);
+                        printf("\n ENDERECO: %s\n", lista[i].inf3);
+                        printf("\n CEP: %s\n", lista[i].inf4);
+                        printf("\n DATA DE NASCIMENTO: %d/%d/%d\n", lista[i].dataD,lista[i].dataM,lista[i].dataA);
+                        printf("\n IDADE: %d\n", checkIdade(lista[i].dataD,lista[i].dataM,lista[i].dataA,ct->tm_mday,ct->tm_mon+1,ct->tm_year+1900));
+                        printf("\n EMAIL: %s\n", lista[i].inf5);
+                        printf("\n DATA DO DIAGNÓSTICO: %d/%d/%d\n", lista[i].diagD,lista[i].diagM,lista[i].diagA);
+                        printf("\n COMORBIDADE: %s\n", lista[i].inf7);
+                        printf("\n\n\t\t");
+                        system("pause");
 
-                        else
-                        {
-                            system("cls");
-                            printf("\n\n\t\t\t\t      EXIBINDO CADASTRO %d\n", i);
-                            printf("\t\t\t       --------------------------------\n\n");
-                            printf(" NOME DO PACIENTE: %s\n", lista[i].inf0);
-                            printf("\n CPF: %s\n", lista[i].inf1);
-                            printf("\n TELEFONE: %s\n", lista[i].inf2);
-                            printf("\n ENDERECO: %s\n", lista[i].inf3);
-                            printf("\n DATA DE NASCIMENTO: %s\n", lista[i].inf4);
-                            printf("\n EMAIL: %s\n", lista[i].inf5);
-                            printf("\n DATA DO DIAGNOSTICO: %s\n", lista[i].inf6);
-                            printf("\n COMORBIDADE: %s\n", lista[i].inf7);
-                            printf("\n\n\t\t");
-
-                            pont_arq= fopen("arquivo.txt", "a");
-
-                            fprintf(pont_arq, "\n NOME DO PACIENTE: %s\n", lista[i].inf0);
-                            fprintf(pont_arq, "\n CPF: %s\n", lista[i].inf1);
-                            fprintf(pont_arq, "\n TELEFONE: %s\n", lista[i].inf2);
-                            fprintf(pont_arq, "\n ENDERECO: %s\n", lista[i].inf3);
-                            fprintf(pont_arq, "\n DATA DE NASCIMENTO: %s\n", lista[i].inf4);
-                            fprintf(pont_arq, "\n EMAIL: %s\n", lista[i].inf5);
-                            fprintf(pont_arq, "\n DATA DO DIAGNOSTICO: %s\n", lista[i].inf6);
-                            fprintf(pont_arq, "\n COMORBIDADE: %s\n", lista[i].inf7);
-                            fprintf(pont_arq, "\n\n------------------------------------------\n\n");
-
-                            fclose (pont_arq);
-
-                            system("pause");
-                        }
                     }
                     break;
 
